255efd4ba08df87cae9d190c3725d5d939a0026f
rajeshwalker <98679393+rajeshwalker@users.noreply.github.com>
2022-11-02 21:23:05 +0500

    Add files via upload
    Signed-off-by: rajeshwalker <98679393+rajeshwalker@users.noreply.github.com>

---
+++ script.js
@@ -0,0 +1,8 @@
+function mOver(obj){
+    obj.innerHTML=" &hearts;Happy Birthday Kowsalya &hearts;"
+    obj.style="color:rgba(240, 239, 220, 0.685); text-align:center; font-size:100px; "
+}
+function mOut(obj){
+    obj.innerHTML=" &hearts;I Hope Your Special Day will Bring You Lots Of Happiness, Love and Fun. You Deserve Them A Lot.BY Lovely&reg;&afr;&jfr;&efr;&sfr;&hfr; &hearts;"
+    obj.style="color:rgba(240, 239, 220, 0.685) text-align:center; font-size:100px"
+}
